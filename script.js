9fcd2d6adcb715dcfb8d7b73c129f5e42ac67eb8
ShruthiThadaka <shruthithadaka1920@gmail.com>
2024-11-20 13:55:03 +0530

    asdf

---
+++ script.js
@@ -0,0 +1,33 @@
+const a = 100;
+console.log(parseInt(a));
+console.log(parseFloat(a))
+console.log(Number.isFinite(a));
+console.log(a.toFixed(2));
+
+const num = 100.234;
+console.log(parseInt(num));
+console.log(parseFloat(num));
+console.log(isFinite(num));
+console.log(num.toFixed(1));
+console.log(num.toPrecision(5));
+console.log(num.toExponential(2));
+console.log(isNaN(num));
+
+const str = "asdf";
+console.log(parseFloat(str));
+console.log(Number.isFinite(str));
+console.log(Number(str).toFixed(2));
+console.log(isNaN(str))
+
+
+const number = "123.123asdf";
+console.log(parseInt(number));
+console.log(parseFloat(number));
+console.log(isNaN(number));
+console.log(Number.isInteger(number));
+console.log(Number(number).toFixed(5));
+
+
+
+
+
